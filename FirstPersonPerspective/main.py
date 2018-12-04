74ed417195efa40ee0cd7c3c074b7eea76bae7f5
Engao <engao@reallusion.com>
2018-12-04 12:20:59 +0800

    [Modify] Add comment and modify calculate rotate way

--- FirstPersonPerspective/main.py
+++ FirstPersonPerspective/main.py
@@ -50,8 +50,7 @@ class MainDialog(PySide2.QtWidgets.QDialog):
         self.lastDragPos = PySide2.QtCore.QPoint()
         self.resize(width, height)
         self.move_offset = move_offset
-        self.first_calculate = False
-
+    
     def keyPressEvent(self, event):
         if event.key() == PySide2.QtCore.Qt.Key_Left:
             camera_move_control(self.move_offset, MoveDirection.LEFT)
@@ -62,30 +61,35 @@ class MainDialog(PySide2.QtWidgets.QDialog):
         elif event.key() == PySide2.QtCore.Qt.Key_Down:
             camera_move_control(self.move_offset, MoveDirection.DOWN)
 
+    def mousePressEvent(self, event):
+        self.lastDragPos = PySide2.QtCore.QPoint(event.pos())
+            
     def mouseMoveEvent(self, event):
         global camera_control
         global camera_transform
         global width
         global height
 
+        # Record previous offset data
         offset_y = self.pixmapOffset.y()
         offset_x = self.pixmapOffset.x()
+        # Calculate new mouse move offset
         self.pixmapOffset += event.pos() - self.lastDragPos
         self.lastDragPos = PySide2.QtCore.QPoint(event.pos())
         offset_x -= self.pixmapOffset.x()
         offset_y -= self.pixmapOffset.y()
 
-        if self.first_calculate:
-            camera_rotate_control(offset_x, offset_y)
-        else:    
-            self.first_calculate = True
+        camera_rotate_control(offset_x, offset_y)
 
-    def mouseReleaseEvent(self, event):        
-        self.first_calculate = False
+    def mouseReleaseEvent(self, event):
+        self.pixmapOffset += event.pos() - self.lastDragPos
+        self.lastDragPos = PySide2.QtCore.QPoint()
 
+    # Using mouse wheel to control camera focal length
     def wheelEvent(self, event):
         global camera_object
         focal_length = camera_object.GetFocalLength(RLPy.RGlobal.GetTime())
+        # Mouse types work in steps of 15 degrees, in which case the delta value is a multiple of 120
         numDegrees = event.delta() / 8
         numSteps = numDegrees / 15.0
         focal_length += numSteps
@@ -97,10 +101,12 @@ def initialize_plugin():
     global camera_transform
     global main_widget
 
+    # Create Camera Control Panel
     camera_pyside_dlg = MainDialog()
     camera_pyside_dlg.setObjectName("Camera First Control")
     camera_pyside_dlg.setWindowTitle("Camera First Control")
 
+    # Add Menu to iClone
     plugin_menu = wrapInstance(int(open_ui_kit.AddMenu("Camera Control", RLPy.EMenu_Plugins)), PySide2.QtWidgets.QMenu)
     plugin_action = plugin_menu.addAction("Open Camera Control")
     plugin_action.triggered.connect(show_dlg)
@@ -110,6 +116,7 @@ def show_dlg():
     camera_setting()
     camera_pyside_dlg.show()
 
+# Find Camera and get camera transform control
 def camera_setting():
     global camera_object
     global camera_control
@@ -123,6 +130,7 @@ def camera_setting():
     camera_transform = RLPy.RTransform.IDENTITY
     camera_control.GetValue(time, camera_transform)
 
+# Calculate the camera rotation by mouse move
 def camera_rotate_control(offset_x, offset_y):
     global camera_control
     rotation_x = 0.0
@@ -131,15 +139,19 @@ def camera_rotate_control(offset_x, offset_y):
     float_control_x = data_block.GetControl('Rotation/RotationX')
     float_control_z = data_block.GetControl('Rotation/RotationZ')
     
+    # Get camera currrent rotation
     temp_x = float_control_x.GetValue(RLPy.RGlobal.GetTime(), rotation_x)
     temp_z = float_control_z.GetValue(RLPy.RGlobal.GetTime(), rotation_z)
     
+    # degree to radians and add offset
     rotation_x = temp_x[1]*180/3.14 + offset_y
     rotation_z = temp_z[1]*180/3.14 + offset_x
     
+    # radians to degree
     float_control_x.SetValue(RLPy.RGlobal.GetTime(), rotation_x/180*3.14)
     float_control_z.SetValue(RLPy.RGlobal.GetTime(), rotation_z/180*3.14)
 
+# Calculate the camera translate by key
 def camera_move_control(offset, direction):
     global camera_transform
     global camera_control
