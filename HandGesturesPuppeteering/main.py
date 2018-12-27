c5ca2e3aef596646aac8a60be435b87b906e28d7
LukeY <44699528+lukeyuRL@users.noreply.github.com>
2018-12-27 19:33:13 +0800

    [modify] add "from PySide2 import *" to avoid unexpected problem

--- HandGesturesPuppeteering/main.py
+++ HandGesturesPuppeteering/main.py
@@ -22,6 +22,7 @@ from handrigger import HandRiggerState
 from handrigger import BlendMode
 
 import PySide2
+from PySide2 import *
 from PySide2.QtCore import *
 from PySide2.QtCore import QResource
 from PySide2.QtGui import *
